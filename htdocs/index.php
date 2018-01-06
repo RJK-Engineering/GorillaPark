8298fe7e243187c6d0a7df2902fed68a27880be9
riccardoparrello <34982861+riccardoparrello@users.noreply.github.com>
2018-01-06 22:40:42 +0100

    Add files via upload
    Eerste upload front-end

---
+++ htdocs/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GorillaPark login</title>
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="stylelogin-screen.css">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+</head>
+
+<body>
+    
+    <div id="content-container">
+    <form action="action/login.php" method="post">
+        Gebruikersnaam:<br>
+        <input type="text" name="username" required autofocus><br>
+        Wachtwoord:<br>
+        <input type="password" name="password" required><br>
+        <input id="log-in-button" type="submit" value="Log in">
+    </form>
+    
+    
+    <?php
+    if (isset($_GET["login_failed"])){
+        echo "<span id='login-fail-text'>Inloggen niet gelukt, probeer het opnieuw.</span>";
+    }    
+    ?>
+    </div>
+    <a href="pages/home.php">Inloggen</a>
+</body>
+</html>
---
+++ htdocs/pages/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GorillaPark home</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+    
+    <div id="content-container">
+    <button>Inchecken</button>
+    </div>
+    
+</body>
+</html>
