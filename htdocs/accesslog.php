7fab28585135502f0dc6b7b40f8bcac234883735
RJK-Engineering <rjklinkhamer@gmail.com>
2018-01-06 23:39:25 +0100

    Version 1

---
+++ htdocs/accesslog.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once('functions.php');
+
+authenticate();
+
+$start = $_GET['startdate'];
+$end = $_GET['enddate'];
+
+if (!isset($start) || !isset($end)) {
+    echo "Something went wrong";
+    exit;
+}
+
+$db = connect_to_db();
+
+try {
+    $sql = "select username, action, date from accesslog where date>=:start and date<=:end";
+    $statement = $db->prepare($sql);
+    $statement->execute(array(
+        ':start' => $start,
+        ':end' => $end
+    ));
+} catch (PDOException $e) {
+    echo "Database error: " . $e->getMessage();
+    exit;
+}
+
+echo '<table>'. PHP_EOL;
+foreach ($statement as $row) {
+    echo sprintf(
+        '<tr><td>%s</td><td>%s</td><td>%s</td></tr>',
+        $row->contact_name,
+        $row->contact_email,
+        $row->contact_modified
+    ) . PHP_EOL;
+}
+echo '</table>'. PHP_EOL;
+
+?>
