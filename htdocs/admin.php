5b1380fce073b0b5896bef99bbff4f1bdb00bb25
riccardoparrello <34982861+riccardoparrello@users.noreply.github.com>
2018-01-07 14:09:20 +0100

    Update admin.php

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -4,65 +4,49 @@ authenticate(array("admin"));
 ?>
 
 <!DOCTYPE html>
+<html>
 <head>
 	<title>Admin User Page</title>
 	<link rel="stylesheet" href="style/main.css">
 	<link rel="stylesheet" href="style/adminuserpage.css">
-	<script src="js/adminuserpage.js"></script>
 </head>
 <body>
-	<div class="container">
-		<div class="title">
-			<h1>Clientlist</h1>
-		</div>
-		<!--adding contacts-->
-		<div class="clientadd"><button id="ClientAdd">+ Add new client</button></div>
-		<!--contact form that needs to be filled in-->
-		<div class="clientaddForm">
-        <form class="spots" action="actions/adduser.php" method="POST">
-			<label for="username">Username</label><input type="text" id="username" class="formFields" placeholder="Username">
-			<label for="password">Password</label><input type="text" id="password" class="formFields" placeholder="Password">
-			<br>
-			<label for="role">Role</label>
-    			<select class="role">
-  					<option value="user">User</option>
-  					<option value="admin">Admin</option>
-  					<option value="service">Service</option>
-				</select>
-			<br><br>
-			<!--buttons for actual addition and cancelling-->
-			<button id="Add">Add Now</button>
-            <!-- <button id="Cancel">Cancel</button> -->
-        </form>
-		</div>
+	
 
-		</div>
-		<div class="title">
-			<h2>Available spots</h2>
-		<div>
-            <form class="spots" action="actions/setoption.php" method="POST">
-                <input type="hidden" name="option" value="total_spots">
-                Total Parking Spots: <input class="spots" type="text" name="value"
-                    value="<?php echo getOption('total_spots'); ?>"><br>
-            </form>
-<!--             <form class="spots" action="actions/setoption.php" method="POST">
-                <input type="hidden" name="option" value="available_spots">
-                Available Parking Spots: <input class="spots" type="text" name="value"
-                    value="from database"><br>
-            </form>
- -->			<br>
-		</div>
-		<br><br><br><br>
-		<div class="title">
-			<h2>Time spent parking</h2>
-		<div>
-			<div class="dateForm">
-            <form class="spots" action="accesslog.php" method="GET">
-    			<label for="datestart">Date Start</label><input type="date" id="datestart" class="formFields">
-    			<label for="datestart">Date End</label><input type="date" id="dateend" class="formFields">
-            </form>
-			</div>
-		</div>
-	</div>
+<h1>Admin</h1>
+    
+<h3>Add User</h3>    
+<form>
+  <label>Name:</label><br>
+  <input type="text" name="name"><br>
+  <label>Password:</label><br>
+  <input type="password" name="password"><br>
+    <input type="radio" name="role" value="user" checked> User
+  <input type="radio" name="role" value="service"> Service
+  <input type="radio" name="role" value="Admin"> Admin<br>
+  <input type="submit" name="submit" value="Submit">
+    
+</form>
+    
+<h3>Parking spots</h3>
+    
+<form>
+  <label>Number:</label><br>
+  <input type="text" name="number" value="23"><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+    
+<h3>Parking log</h3>
+    
+<form>
+  <label>Start date:</label><br>
+  <input type="date" name="startdate"><br>
+  <label>End date:</label><br>
+  <input type="date" name="enddate"><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+    
+    
+    
 </body>
 </html>
